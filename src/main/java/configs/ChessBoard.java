1734043ad377350c658b35666ce8e9c7de39991a
CheesyCrab <1592913644@qq.com>
2022-06-18 19:35:55 +0800

    棋盘的构建

---
+++ src/main/java/configs/ChessBoard.java
@@ -0,0 +1,30 @@
+package configs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.Serial;
+
+public class ChessBoard extends JFrame implements Config {
+    @Serial
+    private static final long serialVersionUID  =1L;
+    public ChessBoard(){
+        setTitle("五子棋AI");
+        setSize(800,800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        for (int i = 0; i < LINES; i++) {
+            g.setColor(Color.black);
+            g.drawLine(X1,X1+SIZE*i,X1+SIZE*(LINES-1),Y1+SIZE*i);
+            g.drawLine(X1 + SIZE * i, X1, Y1 + SIZE * i, Y1 + SIZE * (LINES - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChessBoard();
+    }
+}
---
+++ src/main/java/configs/Config.java
@@ -0,0 +1,9 @@
+package configs;
+
+public interface Config {
+    public static final int X1 = 50;
+    public static final int Y1 = 50;
+    public static final int LINES = 15;
+    public static final int SIZE = 50;
+    public static final int CHESS_SIZE = 30;
+}
